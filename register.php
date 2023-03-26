35160462f8af1cbf8d06cacb2f20b2b9519f0a1f
214g5a0505 <112407428+214g5a0505@users.noreply.github.com>
2023-03-26 19:33:36 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,42 @@
+<?php
+    require '../config/config.php';
+
+
+    if(isset($_POST['register'])) {
+        $errMsg = '';
+        $username = $_POST['username'];
+        $mobile = $_POST['mobile'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $fullname = $_POST['fullname'];
+
+            try {
+                $stmt = $connect->prepare('INSERT INTO users (fullname, mobile, username, email, password) VALUES (:fullname, :mobile, :username, :email, :password)');
+                $stmt->execute(array(
+                    ':fullname' => $fullname,
+                    ':username' => $username,
+                    ':password' => md5($password),
+                    ':email' => $email,
+                    ':mobile' => $mobile,
+                    ));
+                header('Location: register.php?action=joined');
+                exit;
+            }
+            catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+    }
+
+    if(isset($_GET['action']) && $_GET['action'] == 'joined') {
+        $errMsg = 'Registration successfull. Now you can login';
+    }
+?>
 
 <?php include '../include/header.php';?>
+    
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#212529;" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#">To-let</a>
+        <a class="navbar-brand js-scroll-trigger" href="../index.php">To-let</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
@@ -10,7 +44,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav text-uppercase ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="#">Login</a>
+              <a class="nav-link" href="login.php">Login</a>
             </li>
             <li class="nav-item">
             </li>
@@ -23,6 +57,11 @@
         <div class="row">               
               <div class="col-md-8 mx-auto">
                 <div class="alert alert-info" role="alert">
+                    <?php
+                        if(isset($errMsg)){
+                            echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
+                        }
+                    ?>
                     <h2 class="text-center">Register</h2>
                     <form action="" method="post">
                         <div class="row">
